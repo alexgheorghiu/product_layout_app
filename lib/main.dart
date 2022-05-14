7c8beef979edd2f65116f397e28f529968eb3ba2
Alex <alex@scriptoid.com>
2022-05-14 14:41:17 +0300

    Rating stars added

--- lib/main.dart
+++ lib/main.dart
@@ -67,13 +67,14 @@ class ProductBox extends StatelessWidget {
             Image.asset('assets/' + image),
             Expanded(
               child: Container(
-                padding: EdgeInsets.all(5),
+                padding: const EdgeInsets.all(5),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: <Widget>[
                     Text(this.name, style: TextStyle(fontWeight: FontWeight.bold)),
                     Text(this.description),
-                    Text("Price: " + this.price.toString())
+                    Text("Price: " + this.price.toString()),
+                    RatingBox(),
                   ],
                 ),
               ),
@@ -81,6 +82,74 @@ class ProductBox extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+}
+
+class RatingBox extends StatefulWidget{
+  @override
+  _RatingBoxState createState() => _RatingBoxState();
+}
+
+class _RatingBoxState extends State<RatingBox>{
+  int _rating = 0;
+
+  void _setRatingAsOne(){
+    setState(() {
+      _rating = 1;
+    });
+  }
+
+  void _setRatingAsTwo(){
+    setState(() {
+      _rating = 2;
+    });
+  }
+
+  void _setRatingAsThree(){
+    setState(() {
+      _rating = 3;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context){
+    double _size = 20; // star size
+    print(_rating);
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      mainAxisSize: MainAxisSize.max,
+      children: <Widget>[
+        Container(
+          padding: const EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_rating >= 1 ? Icon(Icons.stars, size: _size,) :  Icon(Icons.star_border, size: _size, )),
+            color: Colors.red[500],
+            onPressed: _setRatingAsOne,
+            iconSize: _size,
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_rating >= 2 ? Icon(Icons.stars, size: _size,) :  Icon(Icons.star_border, size: _size, )),
+            color: Colors.red[500],
+            onPressed: _setRatingAsTwo,
+            iconSize: _size,
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_rating >= 3 ? Icon(Icons.stars, size: _size,) :  Icon(Icons.star_border, size: _size, )),
+            color: Colors.red[500],
+            onPressed: _setRatingAsThree,
+            iconSize: _size,
+          ),
+        ),
+      ],
     );
   }
 }
